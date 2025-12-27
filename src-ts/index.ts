45850d26bacf608a6cd308bf4206a4a3a8c6bebf
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-12-27 01:23:26 +0100

    export webview and window types

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,4 +1,8 @@
 import Rod from "./classes/rod";
+import type WebView from "./classes/webview";
+import type Window from "./classes/window";
 export default Rod;
 export { Rod };
 export * from "./types";
+export type { WebView };
+export type { Window };
