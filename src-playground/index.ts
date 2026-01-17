ed4269e143a6b6c1e6d5e00eb717c5c0e8e2c576
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2026-01-17 19:35:01 +0100

    test webview

--- src-playground/index.ts
+++ src-playground/index.ts
@@ -1,18 +1,19 @@
 import Rod from "../src-ts";
 
 const rod = new Rod();
-// const webview = rod.createWebView({
-// 	title: "CoreLauncher - Rod WebView Playground",
-// 	url: "https://example.com",
-// 	focused: false,
-// 	size: {
-// 		width: 400,
-// 		height: 300,
-// 	},
-// });
+const webview = rod.createWebView({
+	title: "CoreLauncher - Rod WebView Playground",
+	url: "https://tauri.app",
+	focused: false,
+	// decorations:false,
+	size: {
+		width: 600,
+		height: 300,
+	},
+});
 
 const tray = rod.createTray({ iconPath: "./icon.ico", title: "hi" });
 tray.on("click", () => {
 	console.log("click");
-	// webview.setVisible(!webview.isVisible);
+	webview.setVisible(!webview.isVisible);
 });
